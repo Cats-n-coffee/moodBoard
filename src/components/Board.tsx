b7ffbca84e8f32b1e98987a3b67bfba1a1434b57
Cats-n-coffee <lucie.macaigne@gmail.com>
2023-06-06 21:48:01 -0400

    add drag drop for elements over board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,3 +1,5 @@
+import { useState, useRef } from 'react';
+import * as _ from 'lodash-es';
 import Card from './Card';
 
 interface BoardProps {
@@ -5,10 +7,67 @@ interface BoardProps {
 }
 
 const Board : React.FC<BoardProps> = ({ cards }) => {
-    return <div className='board-wrapper'>
-        <h2>Board</h2>
-        {cards.map((card, idx) => <Card cardName={card} key={idx} />)}
-    </div>
+    const board = useRef<HTMLDivElement>(null);
+    // Initialize at drag start (start)
+    const [mousePosOnElement, setMousePosOnElement] = useState({ x: 0, y: 0 });
+    // Set position on drop (end)
+    const [elementPos, setElementPos] = useState({ top: '0px', left: '0px'});
+
+    const boardXOffset = 50;
+    const boardYOffset = 100;
+
+    const onDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+        event.preventDefault();
+    }
+
+    const onDrop = (event: React.DragEvent<HTMLDivElement>) => {
+        const mouseXPos = event.pageX - boardXOffset;
+        const mouseYPos = event.pageY - boardYOffset;
+
+        const newCoordX = mouseXPos - mousePosOnElement.x;
+        const newCoordY = mouseYPos - mousePosOnElement.y;
+ 
+        setElementPos({
+            top: `${newCoordY}px`,
+            left: `${newCoordX}px`,
+        })
+    }
+
+    const onDragEnter = (event: React.DragEvent<HTMLDivElement>) => {
+        event.preventDefault();
+    }
+
+    const onDragLeave = (event: React.DragEvent<HTMLDivElement>) => {
+        event.preventDefault();
+    }
+
+    return (
+        <div className='board-wrapper'>
+            <h2>Board</h2>
+            <div
+                className='board-cards-area'
+                ref={board}
+                onDragOver={onDragOver}
+                onDragEnter={onDragEnter}
+                onDragLeave={onDragLeave}
+                onDrop={onDrop}
+            >
+                {
+                cards.map(
+                    (card, idx) => (
+                            <Card
+                                cardName={card}
+                                key={idx}
+                                cardId={idx}
+                                setMousePosOnElement={setMousePosOnElement}
+                                elementPos={elementPos}
+                            />
+                        )
+                    )
+                }
+            </div>
+        </div>
+    )
 }
 
 export default Board;
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,40 @@
 interface CardProps {
     cardName: string,
+    cardId: number,
+    setMousePosOnElement: React.Dispatch<React.SetStateAction<{ x: number, y: number }>>;
+    elementPos: { top: string, left: string },
 }
 
-const Card : React.FC<CardProps> = ({ cardName }) => {
-    return <div className='card-wrapper'>name: {cardName}</div>
+const Card : React.FC<CardProps> = (props) => {
+    const { cardName, cardId, setMousePosOnElement, elementPos } = props;
+    
+    const boardXOffset = 50;
+    const boardYOffset = 100;
+
+    const onDragStart = (event: React.DragEvent<HTMLDivElement>) => {
+        const { top, left } = (event.target as HTMLElement).getBoundingClientRect();
+
+        const mouseXPos = event.pageX - boardXOffset;
+        const mouseYPos = event.pageY - boardYOffset;
+        const elementTop = top - boardYOffset;
+        const elementLeft = left - boardXOffset;
+        // Get the difference between the topCorner and the mouse
+        const differenceX = mouseXPos - elementLeft;
+        const differenceY = mouseYPos - elementTop;
+        setMousePosOnElement({ x: differenceX, y: differenceY });
+    }
+
+    return (
+        <div
+            className='card-wrapper'
+            draggable
+            onDragStart={onDragStart}
+            style={elementPos}
+            data-cardid={cardId}
+        >   
+            name: {cardName}
+        </div>
+    )
 }
 
 export default Card;
